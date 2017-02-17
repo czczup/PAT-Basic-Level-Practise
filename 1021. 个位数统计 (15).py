d275c591d4ee027a19fb3849ec1abc403b393f9f
bibibi <wztxy89@163.com>
2017-02-17 12:52:53 +0800

    1021. 个位数统计 (15)

---
+++ 1021. 个位数统计 (15).py
@@ -0,0 +1,11 @@
+#1021. 个位数统计 (15)
+N = input()
+#用字符串模拟大数
+num = [0,0,0,0,0,0,0,0,0,0]
+for i in range(0,len(N)):
+    num[int(N[i])] = num[int(N[i])] + 1
+for i in range(0,10):
+    if num[i]!=0:   
+        print(i,end='')
+        print(':',end='')
+        print(num[i])
