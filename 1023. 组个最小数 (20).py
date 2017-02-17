c5187c45d2b0b845059e4e1b1a3063e9665fa3bc
bibibi <wztxy89@163.com>
2017-02-17 14:13:51 +0800

    1023. 组个最小数 (20)

---
+++ 1023. 组个最小数 (20).py
@@ -0,0 +1,21 @@
+#1023. 组个最小数 (20)
+import re
+strnum = input()
+amount = re.findall(r"\d+\.?\d*",strnum)
+number = []
+for i in range(0,10):
+    for cnt in range(0,int(amount[i])):
+        number.append(i)
+#print(number)
+number.sort()
+#print(number)
+flag = 0
+for i in range(0,len(number)):
+    if flag == 0:
+        if int(number[i])!=0:
+            print(number[i],end='')
+            print('0'*i,end='')
+            flag = 1
+    else:
+        print(number[i],end='')
+    
