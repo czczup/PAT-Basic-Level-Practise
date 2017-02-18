5c04ea4a6b671f04282332ad7e00ae24f8b82a6a
bibibi <wztxy89@163.com>
2017-02-18 15:50:18 +0800

    1024. 科学计数法 (20)

---
+++ 1024. 科学计数法 (20) .c
@@ -0,0 +1,97 @@
+﻿/*1024. 科学计数法 (20)*/
+#include <stdio.h> 
+#include <string.h>
+void pop(char* listnum,int pos);
+//删除list中下标为pos的元素 
+void append(char* listnum,char ch);
+//在list后追加ch 
+void insert(char* listnum,int pos,char ch);
+//在list的pos下标处插入ch 
+int main()
+{
+    char str[10010],listnum[20000]; 
+    scanf("%s",str);
+    int len = strlen(str);
+    int i = 0;
+
+    int pos = 0;
+    while(str[pos]!='E'){
+        pos++;
+    }//记录E所在的下标 
+    
+    int exp = 0;
+    for(i = pos+2;i<len;i++){
+        exp = exp*10 + (str[i]-'0');
+    } 
+    if(str[pos+1] == '-')
+        exp = -exp;
+    //记录指数的大小 
+    
+    for(i=0;i<pos;i++){
+        listnum[i] = str[i];
+    } 
+    listnum[i] = '\0';
+    //从str中复制得到listnum
+     
+    //数字部分分有小数点和无小数点
+    //指数部分分大于和小于0
+    int decimal = 0;
+    //小数部分长度 
+    if(listnum[2]=='.'){
+        decimal = strlen(listnum) - 3;
+        if(exp > 0)
+            if(exp >= decimal){ 
+                pop(listnum,2);
+                for(i=0;i<exp-decimal;i++){
+                    append(listnum,'0');
+                }
+            } 
+            else{
+                pop(listnum,2);
+                insert(listnum,2 + exp,'.');
+            }
+        else{
+            pop(listnum,2);
+            for(i=0;i<-exp;i++)
+                insert(listnum,1,'0');
+            insert(listnum,2,'.');
+        }
+    }  
+    else{
+        if(exp >= 0)
+            for(i=0;i<exp;i++)
+                append(listnum,'0');
+        else{
+            for(i=0;i<-exp;i++)
+                insert(listnum,1,'0');
+            insert(listnum,2,'.');
+        }
+    }
+    if(listnum[0]=='+'){
+        pop(listnum,0);
+    } 
+    printf("%s",listnum);
+}
+
+
+void pop(char* listnum,int pos)
+{
+    while(listnum[pos]!='\0'){
+        listnum[pos] = listnum[pos+1];
+        pos++;
+    }
+}
+
+void append(char* listnum,char ch){
+    int len = strlen(listnum);
+    listnum[len] = ch;
+    listnum[len+1] = '\0';
+}
+
+void insert(char* listnum,int pos,char ch){
+    int len = strlen(listnum);
+    int i = 0;
+    for(i=len+1;i>pos;i--)
+        listnum[i]=listnum[i-1];
+    listnum[i] = ch;
+}
---
+++ 1024. 科学计数法 (20) 字符型.py
@@ -0,0 +1,37 @@
+#1024. 科学计数法 (20)
+import re
+string = input()
+number = re.findall(r".\d+\.?\d*",string)
+listnum = re.findall(r".",number[0])
+exp = int(number[1])
+#数字部分分有小数点和无小数点
+#指数部分分大于和小于0
+if number[0][2]=='.':
+    decimal = len(number[0]) - 3
+    if exp > 0:
+        if exp >= decimal:
+            listnum.pop(2)
+            for i in range(0,exp - decimal):
+                listnum.append('0')
+        else:
+            listnum.pop(2)
+            listnum.insert(2 + exp,'.')
+    if exp <= 0:
+        listnum.pop(2)
+        for i in range(0,-exp):
+            listnum.insert(1,'0')
+        listnum.insert(2,'.')
+else:
+    if exp >= 0:
+        for i in range(0,exp):
+            listnum.append('0')
+    else:
+        for i in range(0,-exp):
+            listnum.insert(1,'0')
+        listnum.insert(2,'.')
+if listnum[0]=='+':
+    listnum.pop(0)
+str = ('').join(listnum)
+print(str)
+
+
