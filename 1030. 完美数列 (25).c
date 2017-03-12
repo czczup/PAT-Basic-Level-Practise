4fe55ba1b073faff48baa4690237dbfc968f686c
bibibi <wztxy89@163.com>
2017-03-12 10:57:13 +0800

    1030. 完美数列 (25)

--- 1030. 完美数列 (25).c
+++ 1030. 完美数列 (25).c
@@ -1,3 +1,4 @@
+﻿#include <stdio.h>
 int main()
 {
     int N,p,i;
@@ -8,17 +9,21 @@ int main()
     
     for(i=0;i<N;i++){
         int min=chlist[i];
+        //每个chlist[i]轮流当最小值 
         int maxnum=min*p,cnt=0;
         for(int j=0;j<N;j++){
             if(chlist[j]<=maxnum && chlist[j]>=min)
                 cnt++;
         }
         maxlist[i]=cnt;
+        //记录下每次利用数字的数量 
+    }//T(n)=O(n^2),会超时 
     
     int max=maxlist[0];
     for(i=0;i<N;i++){
         if(maxlist[i]>max)
             max=maxlist[i];
+    }//从中选出最大值 
     printf("%d",max);
     
  } 
