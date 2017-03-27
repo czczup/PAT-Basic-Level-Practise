363fee1ba7080dd892302e7476acbd49ec3b586c
bibibi <wztxy89@163.com>
2017-03-27 23:07:50 +0800

    1032. 挖掘机技术哪家强 (20)

---
+++ 1032. 挖掘机技术哪家强 (20).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n,max=0,index=0;
+	int school[100001]={0};//最坏情况有10^5所学校 
+	scanf("%d",&n);
+	while(n--){
+		int num,score;
+		scanf("%d %d",&num,&score);
+		school[num]+=score;
+		if(school[num]>max){
+			max = school[num];
+			index = num;//记录下标 
+		}//在记录数据的同时选出最大值 
+	} 
+	printf("%d %d",index,school[index]);
+}
+
