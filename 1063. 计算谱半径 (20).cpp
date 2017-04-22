4e6f29ece7e8b6b4d49ee7c9181902e7fc0f1a88
bibibi <wztxy89@163.com>
2017-04-22 18:04:51 +0800

    简单数学

---
+++ 1063. 计算谱半径 (20).cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int n;
+	double max = 0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		int a,b;//a+bi
+		scanf("%d %d",&a,&b);
+		double temp = sqrt(a*a + b*b);
+		max = max > temp ? max : temp;	
+	}
+	printf("%lf",max);
+}
+
