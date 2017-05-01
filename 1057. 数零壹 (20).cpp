457e3f9bcfad70aa5985e52e135b766b5fcfa86b
bibibi <wztxy89@163.com>
2017-05-01 19:50:59 +0800

    进制转换

---
+++ 1057. 数零壹 (20).cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char string[100000];
+	gets(string);
+	int len = strlen(string);
+	int sum = 0;
+	for(int i=0;i<len;i++){
+		if(string[i]>='A' && string[i]<='Z')
+			sum += string[i] - 'A' + 1;
+		if(string[i]>='a' && string[i]<='z')
+			sum += string[i] - 'a' + 1;
+	}
+	int one = 0,zero = 0;
+	while(sum!=0){
+		if(sum%2==0)
+			zero++;
+		else
+			one++;
+		sum /= 2;
+	}
+	printf("%d %d",zero,one);
+}
+
