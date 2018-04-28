706479b6baf06d6f733ca7d69ab4cdcbdf02dc86
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:37:59 +0800

    进制转换

--- 1022. D进制的A+B (20).py
+++ 1022. D进制的A+B (20).py
@@ -1,18 +1,12 @@
-#1022. D进制的A+B (20)
-import sys
-A,B,D = map(int,sys.stdin.readline().split())
-number = []
+A,B,D = [int(i) for i in input().split(" ")]
+numbers = []
 sum = A + B
-if sum == 0:
+if sum == 0: # 0的2-10进制数都是0
     print(0)
-#0的2-10进制数都是0
 while sum!=0:
     num = sum % D
-    number.append(num)
-    sum = (sum - num) / D
-    #逆序记录
-number.reverse()
-#翻转
-for i in range(0,len(number)):
-    print(int(number[i]),end='')
- 
+    numbers.append(num)
+    sum = (sum - num) / D #逆序记录
+numbers.reverse() # 翻转
+for number in numbers:
+    print(int(number),end='') # 取整
