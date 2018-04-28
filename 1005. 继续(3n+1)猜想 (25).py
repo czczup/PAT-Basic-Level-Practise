2a63e19bd640be07576abe51f69391020f92a0df
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:34:12 +0800

    散列

--- 1005. 继续(3n+1)猜想 (25).py
+++ 1005. 继续(3n+1)猜想 (25).py
@@ -1,39 +1,29 @@
-#1005. 继续(3n+1)猜想 (25)
-import re
-cnt = int(input())
-string = input()
-inputnum = re.findall(r"\d+\.?\d*",string)
-#从字符串string中提取数字
-listnum = []
-#用于嵌套num的list
-finalnum = []
-#记录结果数字的list
-for i in range(0,cnt):
-    n = int(inputnum[i])
+N = int(input())
+inputNum = [int(i) for i in input().split(" ")]
+callatz = [] # 记录每个数字的Callatz数列
+result = [] # 记录不能其他数字所覆盖的数
+for i in range(N):
+    n = inputNum[i]
     num = []
     while n!=1:
-        if n%2==0:
-            n = n/2
+        if n % 2==0:
+            n = n / 2
         else:
-            n = (3*n+1)/2
-        num.append(int(n))
-    listnum.append(num)
-    #将过程追加在num上
-for i1 in range(0,cnt):
-    n = int(inputnum[i1])
+            n = (3*n+1) / 2
+        num.append(n)
+    callatz.append(num)
+
+for i in range(N):
+    n = inputNum[i]
     flag = 0
-    for i2 in range(0,cnt):
-        if n in listnum[i2]:
+    for j in range(N):
+        if n in callatz[j]: # 判断数字是否被“覆盖”
             flag = 1
-        #判断数字是否出现在过程中
-    if flag==0:
-        finalnum.append(n)
-        #未出现则记录
-finalnum.sort(reverse = True)
-#从大到小排序
-for i in range(0,len(finalnum)):
-    print(finalnum[i],end='')
-    #输出不换行
-    if i!=len(finalnum)-1:
-        print(' ',end='')
-        #最后一个数字后不输出空格
+    if flag==0: # 未被“覆盖”
+        result.append(n)
+
+result.sort(reverse = True) # 从大到小排序
+for i in range(len(result)):
+    print(result[i],end='')
+    if i!=len(result) - 1:
+        print(' ',end='') # 最后一个数字后不输出空格
