f112c9f50c8901030e14340ed1976a57008a577a
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:52:00 +0800

    逻辑题

--- 1066. 图像过滤 (15).cpp
+++ 1066. 图像过滤 (15).cpp
@@ -1,24 +1,22 @@
 #include <stdio.h>
-int main()
-{
-	int M,N,A,B,replace;
-	scanf("%d %d %d %d %d",&M,&N,&A,&B,&replace);
-	int pixel[M][N];
-	for(int i=0;i<M;i++){
-		for(int j=0;j<N;j++){
-			scanf("%d",&pixel[i][j]);
-			if(pixel[i][j]>=A && pixel[i][j]<=B)
-				pixel[i][j]=replace;
-		}
-	} 
-	for(int i=0;i<M;i++){
-		for(int j=0;j<N;j++){
-			printf("%03d",pixel[i][j]);
-			if(j!=N-1)
-				printf(" ");
-			else
-				printf("\n");
-		}
-	}
+int main(){
+    int M,N,A,B,replace;
+    scanf("%d %d %d %d %d",&M,&N,&A,&B,&replace);
+    int pixel[M][N];
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            scanf("%d",&pixel[i][j]);
+            if(pixel[i][j]>=A && pixel[i][j]<=B)
+                pixel[i][j]=replace;
+        }
+    }
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            printf("%03d",pixel[i][j]);
+            if(j!=N-1)
+                printf(" ");
+            else
+                printf("\n");
+        }
+    }
 }
-
