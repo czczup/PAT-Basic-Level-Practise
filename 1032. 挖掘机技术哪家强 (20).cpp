77d4115146efe1cfb8ce16c17b66cabd1568b796
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:41:26 +0800

    查找元素

--- 1032. 挖掘机技术哪家强 (20).cpp
+++ 1032. 挖掘机技术哪家强 (20).cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
-int main()
-{
-	int n,max=0,index=0;
-	int school[100001]={0};//最坏情况有10^5所学校 
-	scanf("%d",&n);
-	while(n--){
-		int num,score;
-		scanf("%d %d",&num,&score);
-		school[num]+=score;
-		if(school[num]>max){
-			max = school[num];
-			index = num;//记录下标 
-		}//在记录数据的同时选出最大值 
-	} 
-	printf("%d %d",index,school[index]);
+int main(){
+    int n,max=0,index=0;
+    int school[100001]={0}; //最多有10^5所学校
+    scanf("%d",&n);
+    while(n--){
+        int num,score;
+        scanf("%d %d",&num,&score);
+        school[num]+=score;
+        if(school[num]>max){
+            max = school[num];
+            index = num; //记录下标
+        } //在记录数据的同时选出最大值
+    }
+    printf("%d %d",index,school[index]);
 }
-
