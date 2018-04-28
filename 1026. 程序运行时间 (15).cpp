adb191bce3082f9172379becaed65e5507a335ca
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:38:33 +0800

    简单模拟

--- 1026. 程序运行时间 (15).cpp
+++ 1026. 程序运行时间 (15).cpp
@@ -1,13 +1,11 @@
-/*1026. 程序运行时间 (15)*/
 #include <stdio.h>
-int main() 
-{
+int main(){
     int CLK1,CLK2,dCLK;
     scanf("%d %d",&CLK1,&CLK2);
-    dCLK = (int)((CLK2-CLK1)/100.0+0.5);
+    dCLK = (int)((CLK2-CLK1) / 100.0 + 0.5);
     int hour,minute,second;
-    hour = dCLK/3600;
-    minute = dCLK%3600/60;
-    second = dCLK%60;
+    hour = dCLK / 3600;
+    minute = dCLK % 3600 / 60;
+    second = dCLK % 60;
     printf("%02d:%02d:%02d",hour,minute,second);
 }
