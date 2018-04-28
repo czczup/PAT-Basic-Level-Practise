672d126fc503e331e04f82f188f4ea1450da1141
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:37:44 +0800

    贪心

--- 1020. 月饼 (25).py
+++ 1020. 月饼 (25).py
@@ -1,40 +1,26 @@
-#1020. 月饼 (25)
-import sys
-import re
-N,D = map(int,sys.stdin.readline().split())
+N,D = [int(i) for i in input().split(" ")]
+cakeSum, profit = 0, 0 #月饼总量,总金额
+# 用int(i)测试点2会报错
+num = [eval(i) for i in input().split(" ")] # 月饼库存数量
+money = [eval(i) for i in input().split(" ")] # 月饼总售价
 cost = []
-sumofcake,moneynum = 0,0
-#月饼总量,总金额
-string1 = input()
-num = re.findall(r"\d+\.?\d*",string1)
-#月饼库存数量
-string2 = input()
-money = re.findall(r"\d+\.?\d*",string2)
-#月饼总售价
-for i in range(0,N):
-    cost.append(eval(money[i])/eval(num[i]))
-#月饼单位价
-for i in range(0,N):
-    maxnum = max(cost) 
-    numidx = cost.index(maxnum)
-    sumofcake = sumofcake + eval(num[numidx])
-    #取最大值,并记录对应索引,直接计入月饼总量
-    if i == 0 and sumofcake > D:
-        moneynum = D * cost[numidx]
+for i in range(N):
+    cost.append(money[i] / num[i]) #月饼单位价
+for i in range(N):
+    maxNum = max(cost) # 取最大值
+    maxIndex = cost.index(maxNum) # 并记录对应索引
+    cakeSum = cakeSum + num[maxIndex] # 计入月饼总量
+    if i == 0 and cakeSum > D: # 一种就够
+        profit = D * cost[maxIndex]
         break
-    #一种就够
-    if sumofcake < D:
-        moneynum = moneynum + eval(money[numidx])
-        #取全部
-    else:
-        moneynum = moneynum + (eval(num[numidx]) - (sumofcake - D)) * maxnum
-        sumofcake = D
-        #取一部分
-    num.pop(numidx)
-    money.pop(numidx)
-    cost.pop(numidx)
-    #删除最大记录
-    if sumofcake == D:
+    if cakeSum < D: # 取全部
+        profit += money[maxIndex]
+    else: # 取一部分
+        profit += (num[maxIndex] - (cakeSum-D)) * maxNum
+        cakeSum = D
+    num.pop(maxIndex)
+    money.pop(maxIndex)
+    cost.pop(maxIndex)
+    if cakeSum == D:
         break
-print("%.2f"%moneynum)
-  
+print("%.2f"%profit)
