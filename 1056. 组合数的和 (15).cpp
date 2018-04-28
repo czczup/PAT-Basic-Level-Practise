7a2cb897d9b1987e095e09df365509d1a5c4118d
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:50:14 +0800

    数学问题

--- 1056. 组合数的和 (15).cpp
+++ 1056. 组合数的和 (15).cpp
@@ -1,21 +1,19 @@
 #include <stdio.h>
-int main()
-{
-	int N;
-	scanf("%d",&N);
-	int number[N];
-	for(int i=0;i<N;i++){
-		scanf("%d",&number[i]);
-	}
-	int sum = 0;
-	for(int i=0;i<N;i++){
-		for(int j=0;j<N;j++){
-			if(i!=j){
-				int temp = number[i]*10 + number[j];
-				sum += temp;
-			}
-		}
-	}
-	printf("%d",sum);
+int main(){
+    int N;
+    scanf("%d",&N);
+    int number[N];
+    for(int i=0;i<N;i++){
+        scanf("%d",&number[i]);
+    }
+    int sum = 0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(i!=j){
+                int temp = number[i]*10 + number[j];
+                sum += temp;
+            }
+        }
+    }
+    printf("%d",sum);
 }
-
