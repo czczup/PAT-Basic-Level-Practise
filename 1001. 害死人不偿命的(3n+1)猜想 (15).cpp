9ed1be347a04cfcef7152883a7a5fa92f794c957
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:32:40 +0800

    简单模拟

--- 1001. 害死人不偿命的(3n+1)猜想 (15).cpp
+++ 1001. 害死人不偿命的(3n+1)猜想 (15).cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>
-int main()
-{
-	int n=0,cnt;
-	scanf("%d",&n);
-	for(cnt=0;n!=1;cnt++){
-		if(n%2==0){
-			n/=2; 
-		}//如果n为偶数
-		 //把n砍掉一半 
-		else{
-			n=3*n+1;
-			n=n/2;
-		}//如果n为奇数
-		 //把3n+1砍掉一半 
-	}
-	printf("%d",cnt);	
-	return 0;
- } 
+﻿#include <stdio.h>
+int main(){
+    int n = 0;
+    scanf("%d",&n);
+    for(int cnt=0;n!=1;cnt++){
+        if(n%2==0){ //如果n为偶数，把n砍掉一半
+            n /= 2;
+        }
+        else{       //如果n为奇数，把3n+1砍掉一半
+            n = 3*n+1;
+            n = n/2;
+        }
+    }
+    printf("%d",cnt);
+    return 0;
+}
