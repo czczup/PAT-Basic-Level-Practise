4bde80b6cf22901736c2bfd7607a6702f05c2913
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:51:02 +0800

    分数化简

--- 1062. 最简分数 (20).py
+++ 1062. 最简分数 (20).py
@@ -5,22 +5,18 @@ def isSimple(a,b):
             return 0
     return 1
 
-
 def main():
     min,max,K = [eval(item) for item in input("").split(" ")]
     if min > max:
         min,max = max,min
-    #一个容易忽略的地方，给定2个正分数，不一定哪个大哪个小
-    #print(min,max,K)
     start,flag = 1,0
     while start/K < max:
-        if start/K > min:
-            if isSimple(start, K):
-                if flag==0:
-                    print("%d/%d"%(start,K),end='')
-                    flag = 1
-                else:
-                    print(" %d/%d"%(start, K), end='')
+        if start/K > min and isSimple(start, K):
+            if flag==0:
+                print("%d/%d"%(start,K),end='')
+                flag = 1
+            else:
+                print(" %d/%d"%(start, K), end='')
         start = start + 1
 
 if __name__ == '__main__':
