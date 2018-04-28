5b9c294fa77a128d2a3bb8a92673063a97d80323
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:41:04 +0800

    two pointers

--- 1030. 完美数列 (25).cpp
+++ 1030. 完美数列 (25).cpp
@@ -1,22 +1,21 @@
 ﻿#include <iostream>
 #include <algorithm>
 using namespace std;
-int main()
-{
-    int n,p;//正整数的个数、给定的参数 
+int main(){
+    int n,p; //正整数的个数、给定的参数
     scanf("%d %d",&n,&p);
     int num[n];
     for(int i=0;i<n;i++){
-    	scanf("%d",&num[i]);
-	}
-	sort(num,num+n);
-	int count = 1,i = 0,j = 0;
-	while(i<n && j<n){//控制i和j的距离最大 
-		while(j<n && num[j]<=(long long)num[i]*p){//max<=min*p
-			count = max(count,j-i+1);
-			j++;
-		}
-		i++; 
-	}
+        scanf("%d",&num[i]);
+    }
+    sort(num,num+n);
+    int count = 1,i = 0,j = 0;
+    while(i<n && j<n){ //控制i和j的距离最大
+        while(j<n && num[j]<=(long long)num[i]*p){//max<=min*p
+            count = max(count,j-i+1);
+            j++;
+        }
+        i++;
+    }
     printf("%d",count);
- } 
+}
