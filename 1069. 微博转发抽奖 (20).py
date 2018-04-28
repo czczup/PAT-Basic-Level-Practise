9b0a538cbd48c40eee8653465f2765b2bfb75967
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:46:54 +0800

    map映射

--- 1069. 微博转发抽奖 (20).py
+++ 1069. 微博转发抽奖 (20).py
@@ -1,13 +1,10 @@
-[m,n,s] = [int(i) for i in input("").split(' ')]
-#print(m,n,s)#转发总量、中奖间隔、第一位中奖者序号
+m,n,s = [int(i) for i in input("").split(' ')] # 转发总量、中奖间隔、第一位中奖者序号
 get = []
-mark = dict() #用字典记录是否中过奖
+mark = dict() # 用字典记录是否中过奖
 for i in range(m):
-    get.append(input(""))
-    mark[get[i]] = False
-    #均标记为未中过奖
-flag = False
-#没有人中奖
+    get.append(input())
+    mark[get[i]] = False # 均标记为未中过奖
+flag = False # 没有人中奖
 i = s - 1
 while i < m:
     if mark[get[i]] == False:
@@ -16,6 +13,6 @@ while i < m:
         flag = True
         i += n
     else:
-        i += 1 #整体后移
+        i += 1 # 整体后移
 if flag==False:
     print("Keep going...")
