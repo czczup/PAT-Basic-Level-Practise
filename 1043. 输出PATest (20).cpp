e37696a5756a22ddd275cd8cc3eefcb4004e0176
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:46:36 +0800

    Hash散列

--- 1043. 输出PATest (20).cpp
+++ 1043. 输出PATest (20).cpp
@@ -1,43 +1,39 @@
-/*1043. 输出PATest (20)*/
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-	char string[10000];
-	char copy[10000]={0};
-	scanf("%s",string);
-	int point[6]={0,1,2,3,4,5};//6个指针分别指向6种字母 
-	int len = strlen(string);
-	for(int i=0;i<len;i++){
-		if(string[i]=='P'){
-			copy[point[0]] = 'P';
-			point[0] = point[0] + 6;
-		}
-		if(string[i]=='A'){
-			copy[point[1]] = 'A';
-			point[1] = point[1] + 6;
-		}
-		if(string[i]=='T'){
-			copy[point[2]] = 'T';
-			point[2] = point[2] + 6;
-		}
-		if(string[i]=='e'){
-			copy[point[3]] = 'e';
-			point[3] = point[3] + 6;
-		}
-		if(string[i]=='s'){
-			copy[point[4]] = 's';
-			point[4] = point[4] + 6;
-		}
-		if(string[i]=='t'){
-			copy[point[5]] = 't';
-			point[5] = point[5] + 6;
-		}
-	}//在一个新的数组内重新按顺序存储 
-	for(int i=0;i<10000;i++){
-	//拷贝字符串离散,可能用到更多空间 
-		if(copy[i]!='\0')
-			printf("%c",copy[i]);
-	}
+int main(){
+    char string[10000];
+    char copy[10000]={0};
+    scanf("%s",string);
+    int point[6]={0,1,2,3,4,5};
+    int len = strlen(string);
+    for(int i=0;i<len;i++){
+        if(string[i]=='P'){
+            copy[point[0]] = 'P';
+            point[0] = point[0] + 6;
+        }
+        if(string[i]=='A'){
+            copy[point[1]] = 'A';
+            point[1] = point[1] + 6;
+        }
+        if(string[i]=='T'){
+            copy[point[2]] = 'T';
+            point[2] = point[2] + 6;
+        }
+        if(string[i]=='e'){
+            copy[point[3]] = 'e';
+            point[3] = point[3] + 6;
+        }
+        if(string[i]=='s'){
+            copy[point[4]] = 's';
+            point[4] = point[4] + 6;
+        }
+        if(string[i]=='t'){
+            copy[point[5]] = 't';
+            point[5] = point[5] + 6;
+        }
+    } //在一个新的数组内重新按顺序存储
+    for(int i=0;i<10000;i++){
+        if(copy[i]!='\0')
+            printf("%c",copy[i]);
+    }
 }
-
--- 1047. 编程团体赛 (20).cpp
+++ 1047. 编程团体赛 (20).cpp
@@ -1,23 +1,21 @@
-/*1047. 编程团体赛 (20)*/
 #include <stdio.h>
-int main()
-{
-	int N;
-	int group[10000]={0};
-	scanf("%d",&N);
-	for(int i=0;i<N;i++){
-		int groupnum,num;
-		int score;
-		scanf("%d-%d %d",&groupnum,&num,&score);
-		group[groupnum] += score;
-	}
-	int max = -1;
-	int mark = 0;
-	for(int i=0;i<10000;i++){
-		if(group[i]>max){
-			max = group[i];
-			mark = i;
-		}
-	}
-	printf("%d %d",mark,group[mark]);
+int main(){
+    int N;
+    int group[10000]={0};
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        int groupnum,num;
+        int score;
+        scanf("%d-%d %d",&groupnum,&num,&score);
+        group[groupnum] += score;
+    }
+    int max = -1;
+    int mark = 0;
+    for(int i=0;i<10000;i++){
+        if(group[i]>max){
+            max = group[i];
+            mark = i;
+        }
+    }
+    printf("%d %d",mark,group[mark]);
 }
