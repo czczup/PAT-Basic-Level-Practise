c0aaa5f0a3204687ca64046f362c659bb3d87ac8
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:36:12 +0800

    字符串处理

--- 1014. 福尔摩斯的约会 (20).cpp
+++ 1014. 福尔摩斯的约会 (20).cpp
@@ -1,38 +1,37 @@
 #include <stdio.h>
-int main()
-{
-	char str[4][61];
-	int cnt;
-	char week[7][5]={"MON","TUE","WED","THU","FRI","SAT","SUN"}; 
-	scanf("%s %s %s %s",&str[0],&str[1],&str[2],&str[3]);
-	for(cnt=0;cnt<61;cnt++){
-		if(str[0][cnt]==str[1][cnt]){
-			if(str[0][cnt]>='A' && str[0][cnt]<='G'){
-					printf("%s ",week[str[0][cnt]-'A']);
-					break;
-			} 
-		}
-	}
-	for(cnt++;cnt<61;cnt++){
-		if(str[0][cnt]==str[1][cnt]){
-			if(str[0][cnt]>='A' && str[0][cnt]<='N'){
-				printf("%02d:",str[0][cnt]-'A'+10);
-				break;
-			}
-			else if(str[0][cnt]>='0' && str[0][cnt]<='9'){
-				printf("%02d:",str[0][cnt]-'0');
-				break;
-			}
-		}
-	}
-	for(cnt=0;cnt<61;cnt++){
-		if(str[2][cnt]==str[3][cnt]){
-			if((str[2][cnt]>='A'&&str[2][cnt]<='Z')||
-			(str[2][cnt]>='a'&& str[2][cnt]<='z')){
-				printf("%02d",cnt);
-				break;
-			}
-		}
-	}
-	return 0;
-} 
+int main(){
+    char str[4][61];
+    char week[7][5]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+    scanf("%s %s %s %s",&str[0],&str[1],&str[2],&str[3]);
+    int cnt;
+    for(cnt=0;cnt<61;cnt++){
+        if(str[0][cnt]==str[1][cnt]){
+            if(str[0][cnt]>='A' && str[0][cnt]<='G'){
+                printf("%s ",week[str[0][cnt]-'A']);
+                break;
+            }
+        }
+    }
+    for(cnt++;cnt<61;cnt++){
+        if(str[0][cnt]==str[1][cnt]){
+            if(str[0][cnt]>='A' && str[0][cnt]<='N'){
+                printf("%02d:",str[0][cnt]-'A'+10);
+                break;
+            }
+            else if(str[0][cnt]>='0' && str[0][cnt]<='9'){
+                printf("%02d:",str[0][cnt]-'0');
+                break;
+            }
+        }
+    }
+    for(cnt=0;cnt<61;cnt++){
+        if(str[2][cnt]==str[3][cnt]){
+            if((str[2][cnt]>='A'&&str[2][cnt]<='Z')||
+            (str[2][cnt]>='a'&& str[2][cnt]<='z')){
+                printf("%02d",cnt);
+                break;
+            }
+        }
+    }
+    return 0;
+}
