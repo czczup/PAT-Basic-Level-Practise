e40617f80273380c8f9d15342e32652b4a05ce9b
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:51:39 +0800

    简单数学

--- 1064. 朋友数 (20).cpp
+++ 1064. 朋友数 (20).cpp
@@ -1,34 +1,25 @@
 ﻿#include <stdio.h>
-int main()
-{
-	//输入部分
-	int N;
-	scanf("%d",&N);
-	int FriNum[N],FriSum[37] = {0}; //下标表示朋友证号
-	for(int i=0;i<N;i++){
-		scanf("%d",&FriNum[i]);
-	}
-	
-	//对每一个数字计数
-	for(int i=0;i<N;i++){
-	    int sum = 0;
-	    while(FriNum[i]!=0){
-	    	sum += FriNum[i] % 10;
-	        FriNum[i] = FriNum[i] / 10;
-		}
-	    FriSum[sum] += 1;
-	}
-	
-	//查找非零项
-	int result[37] = {0},j=0;
-	for(int i=0;i<37;i++)
-	    if(FriSum[i] != 0)
-	        result[j++] = i;
-	
-	//输出部分
-	printf("%d\n",j);
-	printf("%d",result[0]);
-	for(int i=1;i<j;i++)
-	    printf(" %d",result[i]);
+int main(){
+    int N;
+    scanf("%d",&N);
+    int FriNum[N],FriSum[37] = {0}; //下标表示朋友证号
+    for(int i=0;i<N;i++){
+        scanf("%d",&FriNum[i]);
+    }
+    for(int i=0;i<N;i++){ //对每一个数字计数
+        int sum = 0;
+        while(FriNum[i]!=0){
+            sum += FriNum[i] % 10;
+            FriNum[i] = FriNum[i] / 10;
+        }
+        FriSum[sum] += 1;
+    }
+    int result[37] = {0},j=0;
+    for(int i=0;i<37;i++)//查找非零项
+        if(FriSum[i] != 0)
+            result[j++] = i;
+    printf("%d\n",j);
+    printf("%d",result[0]);
+    for(int i=1;i<j;i++)
+        printf(" %d",result[i]);
 }
-
